167391738ceb9fa88b73edfcb166961df62ba55a
Elshad Pirimov <elshadp@gmail.com>
2023-09-21 10:34:34 +0400

    Update TitleServiceImpl.java

--- imdb-search-data/src/main/java/com/imdb/title/service/TitleServiceImpl.java
+++ imdb-search-data/src/main/java/com/imdb/title/service/TitleServiceImpl.java
@@ -6,6 +6,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -23,11 +25,15 @@ public class TitleServiceImpl implements TitleService{
 
     @Cacheable(value = "actors", key = "#actor1+'-'+#actor2")
     public List<String> getTitlesByActors(String actor1, String actor2) {
+        if (actor1 == null || actor1.isEmpty() || actor2 == null || actor2.isEmpty())
+            return Collections.emptyList();
         return dataSearchService.getAllTitlesPlaySameActors(actor1, actor2);
     }
 
     @Cacheable(value = "genre", key = "#genre")
     public Map<String, Rating> getBestTitlesByGenre(String genre) {
+        if (genre == null || genre.isEmpty())
+            return new HashMap<>();
         return dataSearchService.getTitleByGenre(genre);
     }
 
